033804dba1ee23cec669290c874edeec7567d910
Hugo Medina <hmedina.paris75@gmail.com>
2019-05-03 21:30:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Serverless-Swift
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import Serverless_SwiftTests
+
+var tests = [XCTestCaseEntry]()
+tests += Serverless_SwiftTests.allTests()
+XCTMain(tests)
